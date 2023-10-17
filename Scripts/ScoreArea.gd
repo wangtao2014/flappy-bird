31a78eef71e0bd07d072de63ef129b435d3a2967
wangtao <wangtao@ai-beings.com>
2023-10-17 10:44:23 +0800

    delete print

--- Scripts/ScoreArea.gd
+++ Scripts/ScoreArea.gd
@@ -9,7 +9,6 @@ func _on_body_exited(_body):
 	if _body.name == "Bird":  # 先通过body的名字判断一下所撞之物是否为"Bird"
 		AudioManager.play("sfx_point") # 音效
 		GameData.score += 1
-		print("score + 1")
 		#加分以后通过ScreenTree把新的分数更新给所有想接受这个消息的节点
 		get_tree().call_group("SCORE","on_score_updated", GameData.score)
 
